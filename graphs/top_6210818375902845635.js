efa8c4314a89f8e9539ad16dcba959089f1eee38
MarcosPividori <marcos.pividori@gmail.com>
2016-06-01 10:30:01 -0300

    init repo.

---
+++ graphs/top_6210818375902845635.js
@@ -0,0 +1,73 @@
+
+$(document).ready(function() {
+  var options = {
+    chart: {
+        renderTo: '6210818375902845635',
+        type: 'area'
+    },
+    title: {
+        text: 'Overall Timing'
+    },
+    subtitle: {
+        text: 'Hide data series by clicking the legend item.'
+    },
+    xAxis: {
+        labels:
+        {
+          enabled: true,
+          rotation: 0
+        },
+        categories: []
+    },
+    yAxis: {
+        title: {
+            text: 'Time [s]'
+        }
+    },
+    tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.4f} s</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+        },
+        series: {
+            cursor: 'pointer'
+        }
+    },
+
+    series: []
+  };
+
+  $.get('graphs/top_6210818375902845635.csv', function(data) {
+    var lines = data.split('\n');
+    $.each(lines, function(lineNo, line) {
+        var items = line.split(',');
+        if (lineNo == 0) {
+          $.each(items, function(itemNo, item) {
+            if (itemNo > 0) options.xAxis.categories.push(item);
+          });
+        }
+        else {
+          var series = {
+            data: []
+          };
+          $.each(items, function(itemNo, item) {
+            if (itemNo == 0) {
+              series.name = item;
+            } else {
+              series.data.push(parseFloat(item));
+            }
+          });
+          options.series.push(series);
+        }
+    });
+    var chart = new Highcharts.Chart(options);
+  });
+});
